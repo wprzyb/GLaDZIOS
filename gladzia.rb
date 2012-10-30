596030ab4d46f397f844e63549d27245deceac55
skrzyp <jot.skrzyp@gmail.com>
2012-10-30 19:02:19 +0100

    cleaning code and some optimization

--- gladzia.rb
+++ gladzia.rb
@@ -10,31 +10,18 @@
 
 require 'cinch'
 require 'open-uri'
-replies = ['Wszystko wskazuje na to, że tak','Tak','Mam wątpliwości, spytaj ponownie', 'Bez wątpienia',	'Moje źródła mówią, że nie','Sadze, ze raczej tak', 
-			'Ręczę za to', 'Skoncentruj się, i zapytaj jeszcze raz','Perspektywy są raczej słabe','Zdecydowanie, definitywnie tak', 'Teraz lepiej nie mówić', 
-			'Bardzo wątpliwe','Tak, zdecydowanie', 'Tak, to pewne', 'Nie moąna teraz tego przewidzieć', 'Bardzo prawdopodobne','Zadaj pytanie później', 
-			'Odpowiedz brzmi: Nie.', 'Są ku temu dobre perspektywy','Nie licz na to',
-			]
+replies = ['Wszystko wskazuje na to, że tak','Tak','Mam wątpliwości, spytaj ponownie', 'Bez wątpienia',	'Moje źródła mówią, że nie','Sadze, ze raczej tak', 'Ręczę za to', 'Skoncentruj się, i zapytaj jeszcze raz','Perspektywy są raczej słabe','Zdecydowanie, definitywnie tak', 'Teraz lepiej nie mówić', 'Bardzo wątpliwe','Tak, zdecydowanie', 'Tak, to pewne', 'Nie moąna teraz tego przewidzieć', 'Bardzo prawdopodobne','Zadaj pytanie później', 'Odpowiedz brzmi: Nie.', 'Są ku temu dobre perspektywy','Nie licz na to']
 def title(page)
   URI.parse(page).open do |f|
-    f.each_line do |l|
-      if l =~ /<title>\s*(.*)\s*<\/title>/
-        return $1
-      end
-    end
+	 f.each_line do |l| if l =~ /<title>\s*(.*)\s*<\/title>/ then return $1 end end
   end
   nil
 end
-
 def cut(link)
-	if link =~ /(.*)ujeb\.se(.*)/
-	return "already shortened!"
-	else
-	return open('http://ujeb.se/a/add?u=' + link, "UserAgent" => "GLaDZIOS").read
+	if link =~ /(.*)ujeb\.se(.*)/ then return 'niczego, bo już był skrócony :)' else
+		return open('http://ujeb.se/a/add?u=' + link, "UserAgent" => "GLaDZIOS").read
 	end
 end
-
-
 gladzios = Cinch::Bot.new do
 	configure do |c|
 		c.server = 'irc.freenode.org'
@@ -44,50 +31,33 @@ gladzios = Cinch::Bot.new do
 		c.reconnect = true
 		c.port = 6667
 		c.user = 'GLaDZIOS'
-	end
-	
+	end	
 	on :message, '.ping' do |m|
 		m.reply "#{m.user.nick}: pong"
-		debug ":: [ping] by #{m.user.nick}"
 	end
-
 	on :message, '.version' do |m|
-		version = "git-roll"
-		m.reply "GLaDZIOS Hackerspace IRCbot. Version: #{version}"
-		debug ":: [version] by #{m.user.nick}"
+		version = system('git describe --always HEAD').to_s
+		m.reply "Bot ircowy dla Hackerspace Kraków. wersja: #{version}"
 	end
-	
 	on :message, /^.8b (.+)/ do |m|
 		m.reply "#{m.user.nick}: #{replies[rand(replies.length)]}"
-		debug ":: [8b] by #{m.user.nick}"
 	end
-
 	on :message, /^.quit/ do |m|
-		m.reply "Stopping GLaDZIOS..."
+		m.reply "GLaDZIOS idzie spać..."
 		gladzios.quit
 	end
-	
 		on :message, /^.dice (.+)/ do |m, query|
-		m.reply "You rolled : #{rand(query.to_i)+1}"
+		if query == nil then query = 6 end
+		m.reply "Rzut kostką dał: #{rand(query.to_i)+1}"
 	end
-	
 		on :message, /^.pick (.+) (.+)/ do |m, q1, q2|
 		wybory = [q1, q2]
-		m.reply "You'd better choose : #{wybory[rand(wybory.length)]}"
+		m.reply "Lepiej #{wybory[rand(wybory.length)]}"
 	end
-	
 	on :message, /(https?:\/\/[\S]+)/ do |m, query|
-			if title(query) != nil
-				title = title(query)
-			else 
-				title = "Unknown"
-			end
-			
-		m.reply "#{m.user.nick} posted a link to: #{title} - shorten URL: #{cut(query)}"
-	debug ":: [url] #{query} by #{m.user.nick}"
+		if title(query) != nil then title = title(query) else title = "(nieznany)" end
+		m.reply "#{m.user.nick} dał linka do #{title}, a ja skróciłam go do #{cut(query)}"
 	end
 
 end
-
-puts 'Starting GLaDZIOS...'
 gladzios.start
