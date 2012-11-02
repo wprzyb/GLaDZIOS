84a05420b8de1589c4164214e031357389ab35d4
Wejq <wejq@daneos.com>
2012-11-02 01:46:20 +0100

    More and more debuging and messing

--- sqlite.rb
+++ sqlite.rb
@@ -12,21 +12,6 @@
 #require 'open-uri'
 require 'sqlite3'
 
-def check(file)
-
-if !File.exists?(file)
-	database = SQLite3::Database.new file
-end
-
-database = SQLite3::Database.open file
-request = database.prepare('SELECT * FROM sqlite_master WHERE type = "table" ; ')
-query = request.execute
-if query != nil then return 1 else return nil end
-
-database.close if database
-
-end
-
 def exe(file, query)
 	database = SQLite3::Database.open file
 	database.execute(query)
@@ -34,14 +19,39 @@ def exe(file, query)
 end
 
 def sel(file, query)
+	
+	table = 0
 	database = SQLite3::Database.open file
 	request = database.prepare(query)
 	returned = request.execute
 	returned.each do |row|
-    puts row.join "\s"
+    row.join "\s"
+    table = row
 	end
-	return row
-	database.close if database
+	return table	
+	
+	if request then request.close end
+	if database then database.close end
+end
+
+def check(file)
+
+if !File.exists?(file) then
+	database = SQLite3::Database.new file
+	puts "Stworzono Baze"
+end
+
+database = SQLite3::Database.open file
+puts "Otwarto baze"
+
+query = sel(file, 'SELECT * FROM sqlite_master WHERE type = "table" ; ')
+
+puts "Query - " + query.to_s
+if query != nil then return 1 else return nil end
+
+database.close if database
+puts "Zamknieto baze"
+
 end
 
 #########################################################
@@ -54,28 +64,34 @@ end
 														#
 def memo_table_generate(file)							#
 	database = SQLite3::Database.open file				#
-	database.execute "CREATE TABLE IF NOT EXISTS `memo` (`id` int(11) NOT NULL,'time` int(11) NOT NULL,`sender` varchar(10) NOT NULL,`receiver` varchar(10) NOT NULL,`memo` varchar(255) NOT NULL, PRIMARY KEY (`id`);"
-	database.close if database							#
+	database.execute "CREATE TABLE 'memo' ('id' int(11) NOT NULL,'time' int(11) NOT NULL,'sender' varchar(10) NOT NULL,'receiver' varchar(10) NOT NULL,'memo' varchar(255) NOT NULL, PRIMARY KEY ('id'));"
+	puts "Wygenerowano seen"
+	#if database then database.close puts "m-OK." end	#
 end														#
 														#
 def seen_table_generate(file)							#
 	database = SQLite3::Database.open file				#
-	database.execute "CREATE TABLE IF NOT EXISTS `seen` (`id` int(11) NOT NULL,'time` int(11) NOT NULL,`who` varchar(10) NOT NULL,`content` varchar(255) NOT NULL, PRIMARY KEY (`id`);"
-	database.close if database							#
+	database.execute "CREATE TABLE 'seen' ('id' int(11) NOT NULL,'time' int(11) NOT NULL,'who' varchar(10) NOT NULL,'content' varchar(255) NOT NULL, PRIMARY KEY ('id'));"
+	puts "Wygenerowano seen"
+	#if database then database.close puts "s-OK." end	#
 end														#
 #########################################################
 														#
-def seen_check_user(who, where)
+def seen_check_user(who)
+	
+	base = "base.db"
+	
+	sprawdz = check(base)
 
-	if check(where) == nil then seen_table_generate(where) end
-	checked = sel(where, "SELECT * FROM seen WHERE who=#{who}")
-	if checked != nil then return 1 else return nil end
+	if (sprawdz.to_s == nil) then seen_table_generate(base) end
+	checkuser = sel(base, "SELECT * FROM seen WHERE who=#{who}")
+	if checkuser != nil then return 1 else return nil end
 
 end	
 
-def seen_check(who, where)
+def seen_check(who)
 
-	if seen_check_user(who, where) == nil then return "Never seen before"
+	if seen_check_user(who) == nil then return "Never seen before"
 	end
 
 end
@@ -101,5 +117,7 @@ def memo_add(who, what, where)
 end	
 
 =end
-													 
-seen_check_user("skrzyp", "file.db")
+#seen_table_generate("base.db")
+#sel("base.db", 'SELECT * FROM sqlite_master WHERE type = "table" ; ')
+#seen_table_generate("base2.db")
+seen_check_user("Skrzyp")
